e3c72378b7b44377a643997e6f9fa8d83c3cd5c1
Austin Noronha <austin_noronha@hotmail.com>
2020-11-06 19:08:56 +0530

    Created About Web and SV image

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Hero></Hero>
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutUs></AboutUs>      
     </div>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HeroImg from '../assets/hero.jpg';
 import './Hero.css';
 
