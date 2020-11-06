90ffebe82557162c226c9f5f66846c9ed557f392
Austin Noronha <austin_noronha@hotmail.com>
2020-11-06 17:58:20 +0530

    Updated Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import './Hero.css';
 function Hero(){
 
     var heroStyle = {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${HeroImg})`
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${HeroImg})`
     };
 
     return (
